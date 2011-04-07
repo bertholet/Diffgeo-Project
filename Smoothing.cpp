0ce1ee6a60110be83fc06a0a6243642b940c48b8
Peter Bertholet <peter_bertholet@hotmail.com>
2011-04-07 18:02:30 +0200

    endofday commit

--- Smoothing.cpp
+++ Smoothing.cpp
@@ -59,6 +59,19 @@ void Smoothing::smootheMesh_explicitEuler( mesh& m )
 	}
 }
 
+
+/* PARDISO prototype. */
+extern "C" __declspec(dllimport) void pardisoinit (void   *, int    *,   int *, int *, double *, int *);
+extern "C" __declspec(dllimport) void pardiso     (void   *, int    *,   int *, int *,    int *, int *, 
+												   double *, int    *,    int *, int *,   int *, int *,
+												   int *, double *, double *, int *, double *);
+extern "C" __declspec(dllimport) void pardiso_chkmatrix  (int *, int *, double *, int *, int *, int *);
+extern "C" __declspec(dllimport) void pardiso_chkvec     (int *, int *, double *, int *);
+extern "C" __declspec(dllimport) void pardiso_printstats (int *, int *, double *, int *, int *, int *,
+														  double *, int *);
+using namespace std;
+
+
 void Smoothing::smootheMesh_implicitEuler( mesh& m )
 {
 	float lambda= 1, dt= 0.001f;
